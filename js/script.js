b075bc6ebaa1f09f982dd6527ae79545175c2aeb
Chris <borshh1337@gmail.com>
2022-03-10 08:17:42 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello my team");
+function getTime(){
+
+    var currentDate = new Date();
+    var day = currentDate.getDay();
+    var month = currentDate.getMonth();
+    var year = currentDate.getFullYear();
+    var months = ["Iulie" , "August" , "Noiembie"];
+
+    document.getElementById('date').innerHTML = day + " " + month + ", " + year;
+
+}
