dbc3e6ef6c4fbf14501d662706ae571dfca41c48
Chris <borshh1337@gmail.com>
2022-03-16 22:03:45 +0200

    nit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,20 @@
-function getTime(){
-
-    var currentDate = new Date();
-    var day = currentDate.getDay();
-    var month = currentDate.getMonth();
-    var year = currentDate.getFullYear();
-    var months = ["Iulie" , "August" , "Noiembie"];
-
-    document.getElementById('date').innerHTML = day + " " + month + ", " + year;
-
+function startTime()
+{
+var today=new Date();
+var h=today.getHours();
+var m=today.getMinutes();
+var s=today.getSeconds();
+// add a zero in front of numbers<10
+m=checkTime(m);
+s=checkTime(s);
+document.getElementById('ceas').innerHTML=h+":"+m+":"+s;
+t=setTimeout('startTime()',500);
+}
+function checkTime(i)
+{
+if (i<10)
+  {
+  i="0" + i;
+  }
+return i;
 }
